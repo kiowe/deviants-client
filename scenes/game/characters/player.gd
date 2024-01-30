18724b141732904a569aa9828d716e88919a81f7
kiowe <ErickKrause2004@yandex.ru>
2024-01-30 16:15:29 +0300

    improved character movement

--- scenes/game/characters/player.gd
+++ scenes/game/characters/player.gd
@@ -1,12 +1,21 @@
 extends CharacterBody3D
 
-
 @export var speed = 5.0
 @export var accel = 16.0
-@export var jump_velocity = 4.5
+@export var jump_velocity = 5.5
+@export var sensitivity = 0.165
+@export var min_angle = -80
+@export var max_angle = 90
+
+@onready var head = $Head
+@onready var collision_shape_3d = $CollisionShape3D
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var look_rot : Vector2
 
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -25,3 +34,12 @@ func _physics_process(delta):
 		velocity.z = lerp(velocity.z, 0.0, accel * delta)
 
 	move_and_slide()
+	
+	head.rotation_degrees.x = look_rot.x
+	rotation_degrees.y = look_rot.y
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		look_rot.y -= (event.relative.x * sensitivity)
+		look_rot.x -= (event.relative.y * sensitivity)
+		look_rot.x = clamp(look_rot.x, min_angle, max_angle)
